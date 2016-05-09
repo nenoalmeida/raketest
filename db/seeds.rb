befa298054bde8b5fc2c0f699f5816b23a8f792c
NenoAlmeida <nenoxvx@icloud.com>
2016-05-09 06:24:36 -0300

    running rake db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(name: "Mr Usuary", email: "usuary@usuary.com")
